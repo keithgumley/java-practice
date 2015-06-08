7bded81f1f4f513842577b64be5598bf2af2b576
xkeigum <keith.gumley@ammeon.com>
2015-06-08 19:09:17 +0100

    Initial project setup

---
+++ src/main/java/com/keith/java_practice/App.java
@@ -0,0 +1,13 @@
+package com.keith.java_practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
